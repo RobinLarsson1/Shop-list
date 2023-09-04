e593c4e0ab0146910310f1026bce90a19ca016c9
RobinLarsson1 <robin.larsson01@gmail.com>
2023-09-04 14:37:13 +0200

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { AiFillDelete } from 'react-icons/ai';
-import './app.css';
+import '../src/app.css';
 
 
 const ShopList = () => {
